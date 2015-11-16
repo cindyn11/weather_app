270781b973452cb488a1f6fccc7427df5241e2d1
Cindy Neumann <cindy.neumann11@gmail.com>
2015-11-15 21:00:52 -0600

    commit weather app

---
+++ weather.rb
@@ -0,0 +1,29 @@
+#require 'rubygems'
+require 'yahoo_weatherman'
+ 
+def get_location(location)
+  client = Weatherman::Client.new
+  client.lookup_by_location(location)
+end
+
+weather = get_location('54723')
+ 
+today = Time.now.strftime('%w').to_i
+ 
+weather.forecasts.each do |forecast|
+ 
+day = forecast['date']
+ 
+weekday = day.strftime('%w').to_i
+ 
+if weekday == today
+   dayName = 'Today'
+elsif weekday == today + 1
+   dayName = 'Tomorrow'
+else
+   dayName = day.strftime('%^a')
+end
+ 
+puts dayName + ' is going to be ' + forecast['text'].downcase + ' with a low of ' + forecast['low'].to_s + ' and a high of ' + forecast['high'].to_s
+ 
+end
